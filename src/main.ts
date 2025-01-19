f311cbe91e9bc16d9ece7d4d9d006143785dc0f6
Steffen Schanze <steffenjoachim.schanze@gmail.com>
2025-01-19 14:00:09 +0100

    environments added

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
-import { environment } from './environments/environment';
+import { environment } from './environments/environment'; 
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes), 
-    provideFirebaseApp(() => initializeApp(environment.firebase)), 
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), 
     provideFirestore(() => getFirestore()), 
     provideAuth(() => getAuth()), 
   ],
